f2cc1604c7fa66ac4bfcb32c340addbc29adafa6
mystatic4 <80502917+mystatic4@users.noreply.github.com>
2021-10-03 16:47:14 +0800

    Add files via upload

---
+++ qi2/js/SuperSlide_app.js
@@ -0,0 +1,59 @@
+﻿$(document).ready(function(){
+	jQuery(".nav").slide({ type:"menu",titCell:".m",targetCell:".sub",effect:"slideDown",delayTime:300,triggerTime:0,defaultPlay:false,returnDefault:true});
+		/*二级*/jQuery(".sub").slide({ type:"menu",titCell:".m2",targetCell:".sub2",effect:"slideDown",delayTime:300,triggerTime:0,returnDefault:true});
+		/*三级*/jQuery(".sub2").slide({ type:"menu",titCell:".m3",targetCell:".sub3",effect:"slideDown",delayTime:300,triggerTime:0,returnDefault:true});
+		/*追加分级箭头*/$(".m2:has(ul)").add(".m3:has(ul)").find("a:first").append(" &raquo; ");
+	/*导航*/
+	
+	jQuery(".slideBox").slide({mainCell:".bd ul",autoPlay:true,delayTime:700});
+	/*首页幻灯片*/
+
+	
+	jQuery(".picScroll-left").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"leftLoop",autoPlay:true,scroll:1,vis:1});
+	/*首页产品*/
+
+	
+	
+	jQuery(".pro_left").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"leftLoop",autoPlay:true,scroll:1,vis:4});
+	/*产品分类列表*/
+	
+	jQuery(".sideMen").slide({titCell:"h3 i",targetCell:".sideMen_cont",trigger:"click"});
+	/*常见问题*/
+	
+	jQuery(".products_select").slide({type:"menu",titCell:".products_cot_2",targetCell:".hide",effect:"slideDown",delayTime:300,triggerTime:0,defaultPlay:false,returnDefault:true});
+	/*产品筛选*/
+	
+	jQuery(".products_list").slide({type:"menu",titCell:".pic",targetCell:".pic_b",delayTime:300,triggerTime:0,defaultPlay:false,returnDefault:true});
+	/*产品大图*/
+	
+	jQuery(".classify").slide({titCell:"ul li span",targetCell:".child",defaultPlay:false,effect:"slideDown",trigger:"click"});
+	/*侧导航*/
+	
+	jQuery(".pic_list").slide({mainCell:".pic_list_content ul",autoPage:true,effect:"leftLoop",scroll:1,vis:4});
+	/*产品详细小图切换*/
+	
+	jQuery(".products_detail_right").add(".detail").add(".about").add(".honor").slide();
+	/*产品信息*//*产品详细*//*发展历程*//*资质荣誉*/
+	
+	
+	//品牌翻转
+	var turn = function(target,time,opts){
+		target.find('a').hover(function(){
+			$(this).find('img').stop().animate(opts[0],time,function(){
+				$(this).hide().next().show();
+				$(this).next().animate(opts[1],time);
+			});
+		},function(){
+			$(this).find('.info').animate(opts[0],time,function(){
+				$(this).hide().prev().show();
+				$(this).prev().animate(opts[1],time);
+			});
+		});
+	}
+	var verticalOpts = [{'width':0},{'width':'180px'}];
+	turn($('#vertical'),100,verticalOpts);
+	var horizontalOpts = [{'height':0,'top':'120px'},{'height':'240px','top':0}];
+	turn($('#horizontal'),100,horizontalOpts);
+
+	
+});
---
+++ qi2/js/float_app.js
@@ -0,0 +1,23 @@
+//漂浮位置："rm"右中侧;"lm"左中侧;"rb"右下角;"lb"左下角;"r"右上角;"l"左上角;
+
+
+//元素漂浮位置
+	
+	//在线客服
+	$(function(){
+		$(".customer").float({
+			delay : 500,//延迟
+			position:"rm" 
+		});
+	});
+	
+	
+	//IE6提示升级浏览器
+	$(function(){
+		$(".Upgrade_bg").float({
+			delay : 500,//延迟
+			position:"b" 
+		});
+	});
+
+
